e056e2804e78bb7ec6f142cf43ad216e01118ec1
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-04-25 14:28:17 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -120,18 +120,18 @@ with plots:
 		st.map(final_data)
 	m = folium.Map(location=[45.372, -121.6972], zoom_start=12, tiles="Stamen Terrain")
 	folium.Marker(
-    		location=craton_data[LATITUDE], craton_data[LONGITUDE],
+    		location=[craton_data[LATITUDE], craton_data[LONGITUDE]],
 		popup="Cratons",
     		icon=folium.Icon(icon="cloud"),
 	).add_to(m)
 	folium.Marker(
-    		location=cfb_data[LATITUDE],cfb_data[LONGITUDE],
+    		location=[cfb_data[LATITUDE],cfb_data[LONGITUDE]],
 		popup="Continental Flood Basalt"
    		icon=folium.Icon(color="green"),
 	).add_to(m)
 
 	folium.Marker(
-   		location=rift_data[LATITUDE],rift_data[LONGITUDE],
+   		location=[rift_data[LATITUDE],rift_data[LONGITUDE]],
 		popup="Rift Margins"
     		icon=folium.Icon(color="red", icon="info-sign"),
 	).add_to(m)
