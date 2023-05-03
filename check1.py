e39ae17eadda31b22beef59bad09e87a60db9f63
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-03 16:08:41 +0200

    Update check1.py

--- check1.py
+++ check1.py
@@ -22,6 +22,7 @@ for i in sel_tect_unit:
 		l2 = l.fillna(0)
 		df_list.append(l2)
 	df_all_tect_units.append(pd.concat(df_list).reset_index())
+st.write(df_all_tect_units)
 button1=st.expander('See Data')
 if button1:
 	st.write(l2)
