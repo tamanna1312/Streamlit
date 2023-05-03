9cd46ce6d52bd8315f2dc525926d9607ec2ac3bf
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-03 15:46:53 +0200

    Update check1.py

--- check1.py
+++ check1.py
@@ -21,7 +21,7 @@ for i in sel_tect_unit:
 		l2 = l.fillna(0)
 		df_list.append(l2)
 	df_all_tect_units.append(pd.concat(df_list).reset_index())
-st.write(l)
+st.write(df_list)
 
 sel_vis = st.radio('select visualisation', ['map', 'plots'], horizontal=True)
 
