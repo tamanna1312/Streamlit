6a5db97aacd22cbd12d5164b34baf974bf12b0d5
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-10 16:14:45 +0200

    Update check1.py

--- check1.py
+++ check1.py
@@ -78,7 +78,7 @@ else:
 				a=all_data[j][subset_elements]
 				for col in a:
 					p2.line(x='index',y=col,source=a,color=colours[j],line_width=2,legend_label=folders[j])
-				st.bokeh_chart(p2, use_container_width=True)
+		st.bokeh_chart(p2, use_container_width=True)
 				
 				
 		
