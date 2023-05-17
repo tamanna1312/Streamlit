37d2e4624a319591f254030a9fada7bc2123dab0
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-17 09:45:19 +0200

    Update georoc_visual_pt1.py

--- georoc_visual_pt1.py
+++ georoc_visual_pt1.py
@@ -74,10 +74,10 @@ else:
 			title='REE plot',x_range=x_labels, x_axis_label="Element", y_axis_label="Abundance (ppm)")
 			colours = ['blue', 'green', 'purple', 'pink', 'yellow', 'grey', 'black']
 			for j in range(len(folders)):
-				for i in range(0,len(all_data[j])):
-					#a=all_data[i][subset_elements]
-					for col in a:
-						p2.line(x='index',y=col,source=a,color=colours[j],line_width=2,legend_label=folders[j])
+			#for i in range(0,len(all_data[j])):
+			#a=all_data[i][subset_elements]
+				for col in a:
+					p2.line(x='index',y=col,source=a,color=colours[j],line_width=2,legend_label=folders[j])
 			st.bokeh_chart(p2, use_container_width=True)
 		if sel_option == 'CI Normalised':
 			subset_elements=all_data[0].columns.tolist()[119:133]
