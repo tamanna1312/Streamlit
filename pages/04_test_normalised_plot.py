c41f4821c7147a76987df067b4270cc1660dca94
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-26 14:58:36 +0200

    X

--- pages/04_test_normalised_plot.py
+++ pages/04_test_normalised_plot.py
@@ -45,7 +45,7 @@ georoc_data=georoc_file.loc[:,selected_elements]
 x_labels=selected_elements
 p2= figure(
 title=select_options, x_range=x_labels, x_axis_label="Element", y_axis_label="Abundance/CI (ppm)")
-p2.y_range = Range1d(0, 100)
+p2.y_range = Range1d(0, 200)
 final=georoc_data.div(normdata.iloc[0], axis=1)
 for col in final:
 	p2.line(x='index',y=col,source=final,color='blue',line_width=2)
