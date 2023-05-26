a034633bebbe81da1ac981e4a33404c23f7d6d7e
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-26 14:05:50 +0200

    X

--- pages/04_test_normalised_plot.py
+++ pages/04_test_normalised_plot.py
@@ -18,7 +18,7 @@ norm_data=pd.read_csv('norm_data_copy.csv', sep=';',decimal=',')
 st.write(norm_data)
 
 st.header('Lookup Elements')
-df=pd.read_csv("lookup_element_characteristics.csv", encoding='ISO-8859-1')
+df=pd.read_csv("LOOKUP_ELEMENTS_CHARACTERISTICS.csv", encoding='ISO-8859-1')
 
 #st.write(df)
 select_options = st.radio('Select Plot Type', ['REE', 'HSE','VOLATILE(high)','VOLATILE'], horizontal=True)
