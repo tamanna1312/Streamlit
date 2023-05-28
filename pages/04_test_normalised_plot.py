d429d2c7f280e8a2adba4804e07e5ba982d3828d
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-05-28 15:35:55 +0200

    x

--- pages/04_test_normalised_plot.py
+++ pages/04_test_normalised_plot.py
@@ -75,12 +75,12 @@ x_labels=selected_elements
 colours = ['blue', 'green', 'purple', 'pink', 'yellow', 'grey', 'black']
 p2= figure(
 title=select_options, x_range=x_labels, x_axis_label="Element", y_axis_label="Abundance/CI (ppm)")
-p2.y_range = Range1d(0, 200)
+p2.y_range = Range1d(0, 400)
 for j in range(len(folders)):
 	a=st.session_state.all_data[j][selected_elements]
 	final=a.div(normdata.iloc[0], axis=1)
 	for col in final:
-		p2.line(x='index',y=col,source=final,color='blue',line_width=2)
+		p2.line(x='index',y=col,source=final,color=colours[j],line_width=2)
 st.bokeh_chart(p2, use_container_width=True)
 
 
