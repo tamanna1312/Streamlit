82c0be544f9a42b696fe19c8ec6685f609a7ee01
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-09-13 13:41:40 +0200

    Update 01_Visualisations.py

--- pages/01_Visualisations.py
+++ pages/01_Visualisations.py
@@ -126,7 +126,7 @@ else:
 			#title=select_options, x_range=x_labels, x_axis_label="Element",y_axis_label=select_normalising )
 			#p2.y_range = Range1d(0, 500)
 			fig = plt.figure()
-			for j, col in zip(range(len(folders)), colours):
+			for j in range(len(folders)):
 					a=st.session_state.all_data[j][selected_elements]
 				
 					if select_normalising=='CI':
@@ -134,7 +134,7 @@ else:
 						CI_Norm1= CI_Norm.T
 						st.write(a)
 						st.write(CI_Norm1)
-						plt.plot(CI_Norm1[CI_Norm1 != 0], c = col)
+						plt.plot(CI_Norm1[CI_Norm1 != 0], c = colours[j], label = folders)
 						plt.ylabel('sample / CI')
 						#for col in CI_Norm1:
 							#p2.line(x='index',y=col,source=CI_Norm1,color=colours[j],line_width=2,legend_label=folders[j])
@@ -143,7 +143,7 @@ else:
 						st.write(a)
 						st.write(CH_Norm)
 						CH_Norm1 = CH_Norm.T
-						plt.plot(CH_Norm1[CH_Norm1 != 0], c = col)
+						plt.plot(CH_Norm1[CH_Norm1 != 0], c = colours[j], label = folders)
 						plt.legend(folders)
 						plt.ylabel('sample / CH')
 						#for col in CH_Norm:
@@ -151,7 +151,7 @@ else:
 					if select_normalising=='CM':
 						CM_Norm=a.div(normdata.iloc[2], axis=1)
 						CM_Norm1 = CM_Norm.T
-						plt.plot(CM_Norm1[CM_Norm1 != 0], c = col)
+						plt.plot(CM_Norm1[CM_Norm1 != 0], c = colours[j], label = folders)
 						plt.ylabel('sample / CM')
 			
 						#for col in CM_Norm:
