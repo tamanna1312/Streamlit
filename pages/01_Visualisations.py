57ad659265311645d24347f78b0ecf9ee2d4b7cb
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-09-13 14:56:16 +0200

    Update 01_Visualisations.py

--- pages/01_Visualisations.py
+++ pages/01_Visualisations.py
@@ -132,14 +132,14 @@ else:
 					if select_normalising=='CI':
 						CI_Norm=a.div(normdata.iloc[0], axis=1)
 						CI_Norm1= CI_Norm.T
-						plt.plot(CI_Norm1[CI_Norm1 != 0], c = colours[j])
+						plt.plot(CI_Norm1[CI_Norm1 != 0], c = colours[j], label = folders)
 						plt.ylabel('sample / CI')
 						#for col in CI_Norm1:
 							#p2.line(x='index',y=col,source=CI_Norm1,color=colours[j],line_width=2,legend_label=folders[j])
 					if select_normalising=='CH':
 						CH_Norm=a.div(normdata.iloc[1], axis=1)
 						CH_Norm1 = CH_Norm.T
-						plt.plot(CH_Norm1[CH_Norm1 != 0], c = colours[j])
+						plt.plot(CH_Norm1[CH_Norm1 != 0], c = colours[j], label = folders)
 						plt.ylabel('sample / CH')
 						#plt.legend(folders[j])
 						#for col in CH_Norm:
@@ -147,10 +147,9 @@ else:
 					if select_normalising=='CM':
 						CM_Norm=a.div(normdata.iloc[2], axis=1)
 						CM_Norm1 = CM_Norm.T
-						plt.plot(CM_Norm1[CM_Norm1 != 0], c = colours[j])
+						plt.plot(CM_Norm1[CM_Norm1 != 0], c = colours[j], label = folders)
 						plt.ylabel('sample / CM')
 				
-					plt.legend(folders[j])
 					#st.write(labels[j])
 
 						#plt.legend(folders[j])
@@ -161,7 +160,7 @@ else:
 				#st.write(str(folders))
 				#st.write(str(folders[j]))
 			#st.bokeh_chart(p2, use_container_width=True)
-			
+			plt.legend()
 			#plt.show()
 			#plt.legend(labels[j])
 			plt.yscale('log')
