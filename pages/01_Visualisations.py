f26c71991c75be4293633bc8a855c3b01df1e9bc
tamanna1312 <108005745+tamanna1312@users.noreply.github.com>
2023-09-13 14:00:17 +0200

    Update 01_Visualisations.py

--- pages/01_Visualisations.py
+++ pages/01_Visualisations.py
@@ -157,10 +157,10 @@ else:
 						#plt.legend(folders[j])
 						#for col in CM_Norm:
 						#	p2.line(x='index',y=col,source=CM_Norm,color=colours[j],line_width=2,legend_label=folders[j])
-				st.write(folders)
-				st.write(folders[j])
-				plt.legend(folders)
-
+				#st.write(folders)
+				#st.write(folders[j])
+				st.write(str(folders))
+				st.write(str(folders[j]))
 			#st.bokeh_chart(p2, use_container_width=True)
 			
 			#plt.show()
